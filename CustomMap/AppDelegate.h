fb5cadabc708b53d83c906f189faee4419945a36
Carlo Vigiani <mail@viggiosoft.com>
2014-01-19 12:24:04 +0100

    Initial Commit

---
+++ CustomMap/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CustomMap
+//
+//  Created by Carlo Vigiani on 19/Jan/14.
+//  Copyright (c) 2014 viggiosoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
